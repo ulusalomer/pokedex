23327a0755fc4bb982519e5420fde7f86a8b3ab3
Ömer Ulusal <ulusalomer@gmail.com>
2023-11-23 17:15:57 +0000

    Initial Commit

---
+++ PokeIndex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeIndex
+//
+//  Created by Ömer Ulusal on 23/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeIndex/PokeIndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeIndexApp.swift
+//  PokeIndex
+//
+//  Created by Ömer Ulusal on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokeIndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
